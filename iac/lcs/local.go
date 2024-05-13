07a27364db746fdca28fd8671f0405b8fa5f4446
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-05-13 23:49:11 +0200

    iac: Add Pulumi configuration for GitLab mirroring

--- iac/lcs/local.go
+++ iac/lcs/local.go
@@ -78,4 +78,50 @@ func main() {
 		panic(err)
 	}
 	log.Println(up.StdOut)
+
+	// Create a new stack for the mirrored project
+	stackStr2 := "dev-desired-state-mirrored"
+	stackName2 := auto.FullyQualifiedStackName(org, project, stackStr2)
+	workDir2 := filepath.Join("localproject_2")
+
+	stack2, err := auto.NewStackLocalSource(ctx, stackName2, workDir2)
+	if err != nil && auto.IsCreateStack409Error(err) {
+		log.Println("stack " + stackName2 + " already exists")
+	}
+	if err != nil && !auto.IsCreateStack409Error(err) {
+		panic(err)
+	}
+
+	glt := os.Getenv("GITLAB_TOKEN")
+	err = stack2.SetAllConfig(ctx, auto.ConfigMap{
+		"gitlab:token": auto.ConfigValue{
+			Value:  glt,
+			Secret: true,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	refr2, err := stack2.Refresh(ctx)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(refr2.StdOut)
+
+	prev2, err := stack2.Preview(ctx, optpreview.DebugLogging(debug.LoggingOptions{
+		Debug: true,
+	}))
+	if err != nil {
+		panic(err)
+	}
+	log.Println(prev2.StdOut)
+
+	up2, err := stack2.Up(ctx, optup.DebugLogging(debug.LoggingOptions{
+		Debug: true,
+	}))
+	if err != nil {
+		panic(err)
+	}
+	log.Println(up2.StdOut)
 }
