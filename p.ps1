224292d1cf7777565e5e31237915bb2e3502d447
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-05-17 17:58:08 +0200

    chore: Update go.mod files and run go mod tidy and go get -u ./...

--- p.ps1
+++ p.ps1
@@ -1,7 +1,27 @@
-$goModuleDirs = Get-ChildItem -Recurse -Filter "go.mod" | Select-Object -Unique DirectoryName
-foreach ($dir in $goModuleDirs) {
-    Push-Location $dir.DirectoryName
-    go mod tidy
-    go get -u ./...
-    Pop-Location
+begin {
+
+}
+
+process {
+    $yesterday = (Get-Date).AddDays(-1)
+    $goBuildCachePath = Join-Path -Path:$HOME -ChildPath:".cache" -AdditionalChildPath:"go-build"
+    Get-ChildItem -Path:$goBuildCachePath -Recurse:$true -File:$true | Where-Object { $_.LastWriteTime -lt $yesterday } | Remove-Item -Force:$true
+    go clean -cache -modcache
+    if ($IsLinux) {
+        $env:GOCACHE = Join-Path -Path:"/tmp" -ChildPath:"gocache"
+    } elseif ($IsWindows) {
+        Join-Path -Path:$env:TEMP -ChildPath:"gocache"
+    }
+
+    $goModuleDirs = Get-ChildItem -Recurse:$true -Filter:"go.mod"
+    $goModuleDirs | ForEach-Object {
+        Push-Location -Path:$_.DirectoryName
+        go mod tidy
+        go get -u ./...
+        Pop-Location
+    }
+}
+
+end {
+
 }
